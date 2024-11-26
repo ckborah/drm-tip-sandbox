3eae4e04c21dffe5baf709389310b87539a75ff6
Uma Shankar <uma.shankar@intel.com>
2024-11-26 18:57:13 +0530

    drm: Define helper for adding capability property for 1D LUT MULTSEG
    This adds helper functions to create 1D multi-segmented Lut color block
    capabilities. It exposes the hardware block as segments
    which are converted to blob and passed in the property.
    
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/drm_colorop.c
+++ drivers/gpu/drm/drm_colorop.c
@@ -89,7 +89,6 @@ static const struct drm_prop_enum_list drm_colorop_lut3d_interpolation_list[] = 
 };
 
 /* Init Helpers */
-__maybe_unused
 static int drm_create_colorop_capability_prop(struct drm_device *dev,
 					      struct drm_colorop *colorop,
 					      struct drm_property_blob *blob)
@@ -109,6 +108,29 @@ static int drm_create_colorop_capability_prop(struct drm_device *dev,
 				   blob->base.id);
 
 	return 0;
+}
+
+__maybe_unused
+static int drm_colorop_lutcaps_init(struct drm_colorop *colorop,
+				    struct drm_plane *plane,
+				    const struct drm_color_lut_range *ranges,
+				    size_t length)
+{
+	struct drm_device *dev = plane->dev;
+	struct drm_property_blob *blob;
+
+	/* Create Color Caps property for multi-segmented 1D LUT */
+	if (colorop->type != DRM_COLOROP_1D_LUT_MULTSEG)
+		return -EINVAL;
+
+	if (WARN_ON(length == 0 || length % sizeof(ranges[0]) != 0))
+		return -EINVAL;
+
+	blob = drm_property_create_blob(plane->dev, length, ranges);
+	if (IS_ERR(blob))
+		return PTR_ERR(blob);
+
+	return drm_create_colorop_capability_prop(dev, colorop, blob);
 }
 
 static int drm_colorop_init(struct drm_device *dev, struct drm_colorop *colorop,
