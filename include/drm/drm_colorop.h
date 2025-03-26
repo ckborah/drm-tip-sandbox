dd337ae17c0a5f83fc16aa79e20ba73d34a430c6
Alex Hung <alex.hung@amd.com>
2025-03-26 17:47:21 -0600

    drm/colorop: Add 3D LUT support to color pipeline
    It is to be used to enable HDR by allowing userpace to create and pass
    3D LUTs to kernel and hardware.
    
    new drm_colorop_type: DRM_COLOROP_3D_LUT.
    
    Signed-off-by: Alex Hung <alex.hung@amd.com>
    Reviewed-by: Simon Ser <contact@emersion.fr>

--- include/drm/drm_colorop.h
+++ include/drm/drm_colorop.h
@@ -282,6 +282,14 @@ struct drm_colorop {
 	enum drm_colorop_lut1d_interpolation_type lut1d_interpolation;
 
 	/**
+	 * @lut3d_interpolation:
+	 *
+	 * Read-only
+	 * Interpolation for DRM_COLOROP_3D_LUT
+	 */
+	enum drm_colorop_lut3d_interpolation_type lut3d_interpolation;
+
+	/**
 	 * @lut1d_interpolation_property:
 	 *
 	 * Read-only property for DRM_COLOROP_1D_LUT interpolation
@@ -308,6 +316,13 @@ struct drm_colorop {
 	 * Size property for custom LUT from userspace.
 	 */
 	struct drm_property *lut_size_property;
+
+	/**
+	 * @lut3d_interpolation_property:
+	 *
+	 * Read-only property for DRM_COLOROP_3D_LUT interpolation
+	 */
+	struct drm_property *lut3d_interpolation_property;
 
 	/**
 	 * @data_property:
@@ -362,6 +377,11 @@ int drm_colorop_ctm_3x4_init(struct drm_device *dev, struct drm_colorop *colorop
 			     struct drm_plane *plane, bool allow_bypass);
 int drm_colorop_mult_init(struct drm_device *dev, struct drm_colorop *colorop,
 			      struct drm_plane *plane, bool allow_bypass);
+int drm_colorop_3dlut_init(struct drm_device *dev, struct drm_colorop *colorop,
+			   struct drm_plane *plane,
+			   uint32_t lut_size,
+			   enum drm_colorop_lut3d_interpolation_type interpolation,
+			   bool allow_bypass);
 
 struct drm_colorop_state *
 drm_atomic_helper_colorop_duplicate_state(struct drm_colorop *colorop);
@@ -412,6 +432,7 @@ const char *drm_get_colorop_type_name(enum drm_colorop_type type);
  */
 const char *drm_get_colorop_curve_1d_type_name(enum drm_colorop_curve_1d_type type);
 const char *drm_get_colorop_lut1d_interpolation_name(enum drm_colorop_lut1d_interpolation_type type);
+const char *drm_get_colorop_lut3d_interpolation_name(enum drm_colorop_lut3d_interpolation_type type);
 
 void drm_colorop_set_next_property(struct drm_colorop *colorop, struct drm_colorop *next);
 
